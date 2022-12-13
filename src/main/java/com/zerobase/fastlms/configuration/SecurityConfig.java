a743a90e1295414c54c41a27cdcf399b08cedfba
heyazoo1007 <heyazoo1007@gmail.com>
2022-12-13 21:30:52 +0900

    [FEAT] 로그인 로직 시작

---
+++ src/main/java/com/zerobase/fastlms/configuration/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.zerobase.fastlms.configuration;
+
+import com.zerobase.fastlms.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    private final MemberService memberService;
+
+    @Bean
+    UserAuthenticationFailureHandler getFailureHandler() {
+        return new UserAuthenticationFailureHandler();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests()
+                .antMatchers(
+                        "/",
+                        "/member/register",
+                        "/member/email-auth"
+                )
+                .permitAll();
+
+        http.formLogin()
+                .loginPage("/member/login")
+                .failureHandler(getFailureHandler())
+                .permitAll();
+
+        super.configure(http);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.userDetailsService(memberService)
+                .passwordEncoder(getPasswordEncoder());
+
+        super.configure(auth);
+    }
+
+    private PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
