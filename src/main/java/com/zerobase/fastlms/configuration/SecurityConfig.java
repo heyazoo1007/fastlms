0d593f4c9b55ef36ff9d6f9c7b2ec9cbc4fe59b0
heyazoo1007 <heyazoo1007@gmail.com>
2022-12-14 22:10:37 +0900

    [CONFIG] 로그인 설정

--- src/main/java/com/zerobase/fastlms/configuration/SecurityConfig.java
+++ src/main/java/com/zerobase/fastlms/configuration/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -26,18 +27,33 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests()
+        http.csrf().disable();
+
+        http.authorizeRequests()
                 .antMatchers(
                         "/",
                         "/member/register",
-                        "/member/email-auth"
+                        "/member/email-auth",
+                        "/member/find/password"
                 )
                 .permitAll();
+
+        http.authorizeRequests()
+                        .antMatchers("/admin/**")
+                        .hasAuthority("ROLE_ADMIN");
 
         http.formLogin()
                 .loginPage("/member/login")
                 .failureHandler(getFailureHandler())
                 .permitAll();
+
+        http.logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+                        .logoutSuccessUrl("/")
+                        .invalidateHttpSession(true);
+
+        http.exceptionHandling()
+                .accessDeniedPage("/error/denied");
 
         super.configure(http);
     }
